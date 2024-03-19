decd3ffe53387e313e9d268fd642c01cc94300b0
kmh9550599@gmail.com <kmh9550599@gmail.com>
2024-03-19 09:41:17 +0900

    1

--- Test2/5701580_0319_1.c
+++ Test2/5701580_0319_1.c
@@ -19,6 +19,7 @@ int main(void) {
 	double result_rec = factorial_rec(20);
 	
 	stop1 = clock();
+	
 	duration_rec = (double)(stop1 - start1) / CLOCKS_PER_SEC;
 	
 	start2 = clock();
@@ -26,7 +27,9 @@ int main(void) {
 	double result_iter = factorial_iter(20);
 
 	stop2 = clock();
+	
 	duration_iter = (double)(stop2 - start2) / CLOCKS_PER_SEC;
+	
 	printf("20! 재귀함수값 = %f, 반복함수값 = %f\n", result_rec, result_iter);
 	printf("재귀함수 실행시간 = %f, 반복함수 실행시간 = %f\n", duration_rec, duration_iter);
 
